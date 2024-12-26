ce6aa8592d258f0032b6389d29875d4a6c09de6a
Atif Zaman <atifzaman88010@gmail.com>
2024-12-26 23:05:28 +0100

    Region controller Api Route

---
+++ NZWalks.Api/Controllers/RegionsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.Api.Model.Domain;
+
+namespace NZWalks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            var region = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://images.pexels.com/photos/17824133/pexels-photo-17824133/free-photo-of-auckland-city-with-a-view-of-the-sky-tower-new-zealand.jpeg"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://images.pexels.com/photos/27623562/pexels-photo-27623562/free-photo-of-a-city-street-at-night-with-a-long-exposure.jpeg"
+                }
+            };
+
+            return Ok(region);
+
+
+        }
+    }
+}
--- NZWalks.Api/obj/Debug/net8.0/NZWalks.Api.AssemblyInfo.cs
+++ NZWalks.Api/obj/Debug/net8.0/NZWalks.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d5438b74c3f937281f8d5ab42e9b7626440e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
